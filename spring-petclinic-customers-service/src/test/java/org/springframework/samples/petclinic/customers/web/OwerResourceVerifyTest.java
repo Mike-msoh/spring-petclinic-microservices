0fdbf7932b0f0026aacc4b936e132cac77bbb40f
msoh <ohms@kr.ibm.com>
2017-05-16 14:28:49 +0900

    Removed Build errors

--- spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/web/OwerResourceVerifyTest.java
+++ spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/web/OwerResourceVerifyTest.java
@@ -1,18 +1,14 @@
 package org.springframework.samples.petclinic.customers.web;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.util.ArrayList;
+
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.samples.petclinic.customers.model.Owner;
 import org.springframework.samples.petclinic.customers.model.OwnerRepository;
-import org.springframework.samples.petclinic.customers.model.Pet;
 import org.springframework.samples.petclinic.customers.model.PetRepository;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -29,99 +25,92 @@ public class OwerResourceVerifyTest {
     @Autowired
     OwnerRepository ownerRepository;
     
-
-    
-    
-    @Test
-    public void shouldGetAPet() throws Exception {
-        Pet pet = petRepository.findOne(2);
-        assertThat(petRepository.findAll()).contains(pet);
-    }
     
     /*
      * OwerData Format Test
      */
     
-//    @Test(expected = IndexOutOfBoundsException.class)
-//    public void testEmptyList() {
-//        new ArrayList<>().get(0);
-//    }
-//    
-//    @Test(expected = IllegalArgumentException.class)
-//    public void shouldOwerNeed10DigitTelephonFormat() throws Exception {
-//        Owner owner = new Owner();
-//        owner.setFirstName("George");
-//        owner.setLastName("Bush");
-//        owner.setCity("Seoul");
-//        owner.setAddress("IFC IBM");
-//        owner.setTelephone("12345678999999");
-//        ownerResource.createOwner(owner);
-//    }
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testEmptyList() {
+        new ArrayList<>().get(0);
+    }
     
-//    @Test(expected = IllegalArgumentException.class)
-//    public void owerNeedTelephonNumberFormat() throws Exception {
-//        Owner owner = new Owner();
-//        owner.setFirstName("George");
-//        owner.setLastName("Bush");
-//        owner.setCity("Seoul");
-//        owner.setAddress("IFC IBM");
-//        owner.setTelephone("02-3781-1234");
-//        ownerResource.createOwner(owner);
-//    }
-//    
-//    
-//    /*
-//     * Ower Data Mandatory Resource Test
-//     */
-//
-//    @Test(expected = IllegalArgumentException.class)
-//    public void owerNeedFirstname() throws Exception {
-//        Owner owner = new Owner();
-//        owner.setLastName("Bush");
-//        owner.setCity("Seoul");
-//        owner.setAddress("IFC IBM");
-//        owner.setTelephone("123456789");
-//        ownerResource.createOwner(owner);
-//    }
-//    
-//    @Test(expected = IllegalArgumentException.class)
-//    public void owerNeedLastname() throws Exception {
-//        Owner owner = new Owner();
-//        owner.setFirstName("George");
-//        owner.setCity("Seoul");
-//        owner.setAddress("IFC IBM");
-//        owner.setTelephone("123456789");
-//        ownerResource.createOwner(owner);
-//    }
-//    
-//    @Test(expected = IllegalArgumentException.class)
-//    public void owerNeedAddress() throws Exception {
-//        Owner owner = new Owner();
-//        owner.setFirstName("George");
-//        owner.setLastName("Bush");
-//        owner.setCity("Seoul");
-//        owner.setTelephone("123456789");
-//        ownerResource.createOwner(owner);
-//    }
-//    
-//    @Test(expected = IllegalArgumentException.class)
-//    public void owerNeedCityName() throws Exception {
-//        Owner owner = new Owner();
-//        owner.setFirstName("George");
-//        owner.setLastName("Bush");
-//        owner.setAddress("IFC IBM");
-//        owner.setTelephone("123456789");
-//        ownerResource.createOwner(owner);
-//    }
-//    
-//    @Test(expected = IllegalArgumentException.class)
-//    public void owerNeedTelephonNumber() throws Exception {
-//        Owner owner = new Owner();
-//        owner.setFirstName("George");
-//        owner.setLastName("Bush");
-//        owner.setCity("Seoul");
-//        owner.setAddress("IFC IBM");
-//        ownerResource.createOwner(owner);
-//    }
+    @Test(expected = javax.validation.ConstraintViolationException.class)
+    public void shouldOwerNeed10DigitTelephonFormat() {
+   
+        Owner owner = new Owner();
+        owner.setFirstName("George");
+        owner.setLastName("Bush");
+        owner.setCity("Seoul");
+        owner.setAddress("IFC IBM");
+        owner.setTelephone("12345678999999");
+        ownerRepository.save(owner);
+    }
+    
+    @Test(expected = javax.validation.ConstraintViolationException.class)
+    public void owerNeedTelephonNumberFormat() {
+        Owner owner = new Owner();
+        owner.setFirstName("George");
+        owner.setLastName("Bush");
+        owner.setCity("Seoul");
+        owner.setAddress("IFC IBM");
+        owner.setTelephone("02-3781-1234");
+        ownerRepository.save(owner);
+    }
+    
+    
+    /*
+     * Ower Data Mandatory Resource Test
+     */
+
+    @Test(expected = javax.validation.ConstraintViolationException.class)
+    public void owerNeedFirstname() {
+        Owner owner = new Owner();
+        owner.setLastName("Bush");
+        owner.setCity("Seoul");
+        owner.setAddress("IFC IBM");
+        owner.setTelephone("123456789");
+        ownerRepository.save(owner);
+    }
+    
+    @Test(expected = javax.validation.ConstraintViolationException.class)
+    public void owerNeedLastname() {
+        Owner owner = new Owner();
+        owner.setFirstName("George");
+        owner.setCity("Seoul");
+        owner.setAddress("IFC IBM");
+        owner.setTelephone("123456789");
+        ownerRepository.save(owner);
+    }
+    
+    @Test(expected = javax.validation.ConstraintViolationException.class)
+    public void owerNeedAddress() {
+        Owner owner = new Owner();
+        owner.setFirstName("George");
+        owner.setLastName("Bush");
+        owner.setCity("Seoul");
+        owner.setTelephone("123456789");
+        ownerRepository.save(owner);
+    }
+    
+    @Test(expected = javax.validation.ConstraintViolationException.class)
+    public void owerNeedCityName() {
+        Owner owner = new Owner();
+        owner.setFirstName("George");
+        owner.setLastName("Bush");
+        owner.setAddress("IFC IBM");
+        owner.setTelephone("123456789");
+        ownerRepository.save(owner);
+    }
+    
+    @Test(expected = javax.validation.ConstraintViolationException.class)
+    public void owerNeedTelephonNumber() {
+        Owner owner = new Owner();
+        owner.setFirstName("George");
+        owner.setLastName("Bush");
+        owner.setCity("Seoul");
+        owner.setAddress("IFC IBM");
+        ownerRepository.save(owner);
+    }
 	
 }
--- spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/web/PetResourceVerifyTest.java
+++ spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/web/PetResourceVerifyTest.java
@@ -1,22 +1,16 @@
 package org.springframework.samples.petclinic.customers.web;
 
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.Date;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.samples.petclinic.customers.model.Owner;
 import org.springframework.samples.petclinic.customers.model.OwnerRepository;
 import org.springframework.samples.petclinic.customers.model.Pet;
@@ -24,38 +18,27 @@ import org.springframework.samples.petclinic.customers.model.PetRepository;
 import org.springframework.samples.petclinic.customers.model.PetType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
 
 /**
  * @author msoh
  */
 @RunWith(SpringRunner.class)
-@WebMvcTest(PetResource.class)
+@DataJpaTest
 @ActiveProfiles("test")
 public class PetResourceVerifyTest {
 
     @Autowired
-    private MockMvc mvc;
-
-    @MockBean
     PetRepository petRepository;
 
-    @MockBean
-    OwnerRepository ownerRepository;
-
-    @Test // (expected = NullPointerException.class)
-    public void shouldSaveProperDateFormat() {
-    	
-        // Create Null Date 
-        Date bDate = null;
+    @Test 
+    public void shouldAddPetWithProperElement() {
         
         Owner owner = new Owner();
         owner.setFirstName("George");
         owner.setLastName("Bush");
         
         Pet pet = new Pet();
-        pet.setBirthDate(bDate);
         pet.setName("testDog");
         pet.setId(8);
         
@@ -64,74 +47,20 @@ public class PetResourceVerifyTest {
         pet.setType(petType);
         
         owner.addPet(pet);
-    }
-    
-//    @Test
-//    public void shouldSaveProperFormat() {
-//    	
-//        Date bDate = new Date();
-//       
-//        Owner owner = new Owner();
-//        owner.setFirstName("George");
-//        owner.setLastName("Bush");
-//        owner.setAddress("IFC IBM");
-//        owner.setTelephone("123456789");
-//        owner.setCity("Seoul");
-//        
-//        Pet pet = new Pet();
-//        pet.setBirthDate(bDate);
-//        pet.setName("testDog");
-//        pet.setId(8);
-//        
-//        PetType petType = new PetType();
-//        petType.setId(6);
-//        pet.setType(petType);
-//        
-//        owner.addPet(pet);
-//        
-//        assertThat(petRepository.findAll()).contains(pet);
-//    }
+        List<Pet> pList = owner.getPets();
         
-    
-//    @Test
-//    public void shouldGetAPet() throws Exception {
-//      Pet pet = petRepository.findOne(2);
-//      assertEquals("Basil", pet.getName());
-//    }
-    
+        assertEquals(asList(pet), (pList));
+    }
+ 
     @Test
-    public void shouldGetAPetInJSonFormat() throws Exception {
-
-        Pet pet = setupPet();
-
-        given(petRepository.findOne(2)).willReturn(pet);
-
-
-        mvc.perform(get("/owners/2/pets/2.json").accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType("application/json;charset=UTF-8"))
-            .andExpect(jsonPath("$.id").value(2))
-            .andExpect(jsonPath("$.name").value("Basil"))
-            .andExpect(jsonPath("$.type.id").value(6));
+    public void shouldGetAPet() throws Exception {
+      Pet pet = petRepository.findOne(2);
+      assertThat(petRepository.findAll()).contains(pet);
+      Pet pet3 = petRepository.findOne(3);
+      assertThat(petRepository.findAll()).contains(pet3);
+      Pet pet4 = petRepository.findOne(4);
+      assertThat(petRepository.findAll()).contains(pet4);
+      Pet pet5 = petRepository.findOne(5);
+      assertThat(petRepository.findAll()).contains(pet5);
     }
-
-    private Pet setupPet() {
-        Owner owner = new Owner();
-        owner.setFirstName("George");
-        owner.setLastName("Bush");
-
-        Pet pet = new Pet();
-
-        pet.setName("Basil");
-        pet.setId(2);
-
-        PetType petType = new PetType();
-        petType.setId(6);
-        pet.setType(petType);
-
-        owner.addPet(pet);
-        return pet;
-    }
-    
-
 }
