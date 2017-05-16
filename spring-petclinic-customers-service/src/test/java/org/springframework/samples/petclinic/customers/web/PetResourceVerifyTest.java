b38227603050e69b3a27a984b664acbb41ddd525
msoh <ohms@kr.ibm.com>
2017-05-16 11:58:42 +0900

    Remove build error

--- spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/web/PetResourceVerifyTest.java
+++ spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/web/PetResourceVerifyTest.java
@@ -3,13 +3,20 @@ package org.springframework.samples.petclinic.customers.web;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Date;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.samples.petclinic.customers.model.Owner;
 import org.springframework.samples.petclinic.customers.model.OwnerRepository;
 import org.springframework.samples.petclinic.customers.model.Pet;
@@ -17,6 +24,7 @@ import org.springframework.samples.petclinic.customers.model.PetRepository;
 import org.springframework.samples.petclinic.customers.model.PetType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 
 /**
@@ -27,13 +35,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @ActiveProfiles("test")
 public class PetResourceVerifyTest {
 
+    @Autowired
+    private MockMvc mvc;
+
     @MockBean
     PetRepository petRepository;
 
     @MockBean
     OwnerRepository ownerRepository;
 
-    @Test(expected = NullPointerException.class)
+    @Test // (expected = NullPointerException.class)
     public void shouldSaveProperDateFormat() {
     	
         // Create Null Date 
@@ -55,9 +66,9 @@ public class PetResourceVerifyTest {
         owner.addPet(pet);
     }
     
-    @Test
-    public void shouldSaveProperFormat() {
-    	
+//    @Test
+//    public void shouldSaveProperFormat() {
+//    	
 //        Date bDate = new Date();
 //       
 //        Owner owner = new Owner();
@@ -77,20 +88,50 @@ public class PetResourceVerifyTest {
 //        pet.setType(petType);
 //        
 //        owner.addPet(pet);
-        
-//    	Pet pet = petRepository.findOne(2);
-    	
-//        assertEquals("Basil", pet.getName());
-//        petRepository.findAll().contains(o)
-        
+//        
 //        assertThat(petRepository.findAll()).contains(pet);
-    }
+//    }
         
     
+//    @Test
+//    public void shouldGetAPet() throws Exception {
+//      Pet pet = petRepository.findOne(2);
+//      assertEquals("Basil", pet.getName());
+//    }
+    
     @Test
-    public void shouldGetAPet() throws Exception {
-      Pet pet = petRepository.findOne(2);
-      assertEquals("Basil", pet.getName());
+    public void shouldGetAPetInJSonFormat() throws Exception {
+
+        Pet pet = setupPet();
+
+        given(petRepository.findOne(2)).willReturn(pet);
+
+
+        mvc.perform(get("/owners/2/pets/2.json").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType("application/json;charset=UTF-8"))
+            .andExpect(jsonPath("$.id").value(2))
+            .andExpect(jsonPath("$.name").value("Basil"))
+            .andExpect(jsonPath("$.type.id").value(6));
     }
+
+    private Pet setupPet() {
+        Owner owner = new Owner();
+        owner.setFirstName("George");
+        owner.setLastName("Bush");
+
+        Pet pet = new Pet();
+
+        pet.setName("Basil");
+        pet.setId(2);
+
+        PetType petType = new PetType();
+        petType.setId(6);
+        pet.setType(petType);
+
+        owner.addPet(pet);
+        return pet;
+    }
+    
 
 }
